2406d108814766ba2e301ef4f941836baa455e98
junthecoder <>
2023-03-01 13:24:10 +0900

    Add a model: Menu

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function menus()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
